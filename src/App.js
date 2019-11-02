cbe89d3426f8b05fb4eb2562a5179802192f21b6
Catherine Marquez <mrs.cmmarquez@gmail.com>
2019-11-02 04:48:22 -0400

    Shop Page, Collection Preview and Collection Item
    Shop Page, Collection Preview and Collection Item

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
+import ShopPage from './pages/shop/shop.component';
+import './App.css';
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route component={HomePage} exact path="/"/>
-                <Route component={HatsPage} path="/shop/hats"/>
+                <Route component={ShopPage} path="/shop"/>
             </Switch>
         </div>
     );
