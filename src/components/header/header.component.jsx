9f962e3606ee1a95fec07c2d1a884383d5ad0ebf
Catherine Marquez <mrs.cmmarquez@gmail.com>
2019-11-05 02:04:11 -0500

    Redux for User
    Redux for User

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
@@ -10,6 +11,11 @@ const Header = ({ currentUser }) => (
             <Logo className='logo'/>
         </Link>
         <div className='options'>
+            {currentUser ?
+                <Link className='option' to='/shop'>
+                    Welcome, {currentUser.displayName}
+                </Link>
+                : ''}
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
@@ -29,4 +35,8 @@ const Header = ({ currentUser }) => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
