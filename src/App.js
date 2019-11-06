120f422846b8f4995b3494d48773f085a3f3f9bf
Catherine Marquez <mrs.cmmarquez@gmail.com>
2019-11-06 02:36:38 -0500

    Checkout Components
    Checkout Components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -42,6 +43,7 @@ class App extends React.Component {
                 <Switch>
                     <Route component={HomePage} exact path='/'/>
                     <Route component={ShopPage} path='/shop'/>
+                    <Route component={CheckoutPage} exact path='/checkout'/>
                     <Route exact path='/signin'
                            render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInAndSignUpPage/>)}/>
                 </Switch>
