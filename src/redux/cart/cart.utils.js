6a4ad05072c709ff5f45fefdc3de85328ce043b7
Catherine Marquez <mrs.cmmarquez@gmail.com>
2019-11-05 20:03:26 -0500

    Remove console.log
    Remove console.log

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,6 +1,5 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
     const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-    console.log("existingCartItem", existingCartItem);
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
